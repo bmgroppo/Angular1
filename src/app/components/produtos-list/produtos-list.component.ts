c014c54c3d090ac78b3f14ec8d8efe62850ea893
Bruno Groppo <bmgroppo@gmail.com>
2025-05-17 22:28:21 -0300

    Primeiro

---
+++ src/app/components/produtos-list/produtos-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produtos-list',
+  imports: [],
+  templateUrl: './produtos-list.component.html',
+  styleUrl: './produtos-list.component.css'
+})
+export class ProdutosListComponent {
+
+}
